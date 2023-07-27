8beda6003d309e8dae370a51e62d6e5168e78d95
ayushsachan123 <ayushsachan.in@gmail.com>
2023-07-27 20:10:13 +0530

    add Reverse.cpp

---
+++ Linked_List/Singly_Linked_List/Reverse.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+	int data;
+    Node *next;
+    Node(int x){
+    	data = x;
+    	next = NULL;
+    }
+};
+
+//Naive Solution  Space O(n) --Using vector
+Node *revList(Node *head){
+	vector<int> arr;
+	Node *ptr = head;
+	while(ptr!=NULL){
+		arr.push_back(ptr->data);
+		ptr = ptr->next;
+	}
+	ptr = head;
+	while(ptr!=NULL){
+		ptr->data = arr.back();
+		arr.pop_back();
+		ptr = ptr->next;
+	}
+	return head;
+}
+
+//Efficient Solution --Changing links of the Node
+Node *revListE(Node *head){
+	Node *curr = head;
+	Node *prev = NULL;
+
+	while(curr!=NULL){
+		Node *next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	return prev;
+}
+
+
+void printList(Node *head){
+if(head == NULL)
+	return; 
+
+cout<<head->data<<" ";
+printList(head->next);
+
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+Node *head = new Node(10);
+head->next = new Node(20);
+head->next->next = new Node(30);                                                                                                                                                                                                                                           
+head->next->next->next = new Node(40);
+
+//head = revList(head);
+head = revListE(head);
+printList(head);
+
+return 0;
+} 
