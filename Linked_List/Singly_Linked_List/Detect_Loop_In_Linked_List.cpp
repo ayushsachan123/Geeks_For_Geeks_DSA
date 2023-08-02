21635723cb3680f5d919d28a544da96dcf60b160
ayushsachan123 <ayushsachan.in@gmail.com>
2023-08-02 20:11:20 +0530

    add Detect_Loop_In_Linked_List.cpp

---
+++ Linked_List/Singly_Linked_List/Detect_Loop_In_Linked_List.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+	int data;
+	Node *next;
+	bool visited;
+	Node(int d){
+		data =d;
+		next = NULL;
+		visited = false;
+	}
+};
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+
+
+return 0;
+} 
