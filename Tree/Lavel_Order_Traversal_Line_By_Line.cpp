c7f58c26f055bb1fbb8be667a44d5b85f793aeb3
ayushsachan123 <ayushsachan.in@gmail.com>
2023-08-28 18:11:20 +0530

    add Lavel_Order_Traversal_Line_By_Line.cpp

---
+++ Tree/Lavel_Order_Traversal_Line_By_Line.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+	int data;
+	Node *left;
+	Node *right;
+	Node(int k){
+		data = k;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+void levelOrder(Node *root){
+   if(root == NULL) return;
+   queue<Node *>q;
+   q.push(root);
+   q.push(NULL);
+   while(q.size()>1){
+   	Node *curr = q.front();
+   	q.pop();
+   	if(curr == NULL){
+   		cout<<"\n";
+   		q.push(NULL);
+   		continue;
+   	}
+   	
+   	cout<<curr->data<<" ";
+   	if(curr->left != NULL){
+   		q.push(curr->left);
+   	}
+   	if(curr->right != NULL){
+   		q.push(curr->right);
+   	}
+   } 
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+Node *root = new Node(10);
+root->left = new Node(20);
+root->right = new Node(30);
+root->left->left = new Node(40);
+
+levelOrder(root);
+
+return 0;
+} 
