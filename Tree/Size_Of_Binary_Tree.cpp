de2032388ac465898858c5b92de3f473a31c4a06
ayushsachan123 <ayushsachan.in@gmail.com>
2023-10-01 20:10:33 +0530

    add Size_Of_Binary_Tree.cpp

---
+++ Tree/Size_Of_Binary_Tree.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+	int data;
+	Node *left;
+	Node *right;
+	Node(int k){
+		data = k;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+int size(Node *root){
+	if(root == NULL) return 0;
+	return (1+ size(root->left) + size(root->right));
+}
+
+int main(){
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+Node *root = new Node(10);
+root->left = new Node(20);
+root->right = new Node(30);
+root->left->left = new Node(40);
+
+int ans = size(root);
+cout<<ans<<endl;
+
+return 0;
+} 
